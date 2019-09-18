85bd1f2ac66034e2a900c3681b982c7ce896c273
Nicolas Widart <n.widart@gmail.com>
2019-09-18 10:39:13 +0200

    First commit

---
+++ cypress/integration/cy-server.spec.js
@@ -0,0 +1,13 @@
+context('Cy Server Randomness', () => {
+  beforeEach(() => {
+    cy.visit('https://hub.docker.com/_/php?tab=tags')
+  })
+
+  it('something', () => {
+    cy.server();
+    cy.route("get", "https://hub.docker.com/api/content/v1/products/images/php", "fixture:php.json")
+      .as('dockerPhp');
+
+    cy.get('[data-testid=productHeader_productName]').should('contain', "php");
+  });
+});
