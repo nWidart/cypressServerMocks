470a7942c733b0f4e477d1da1f2b4f929f298499
Nicolas Widart <n.widart@gmail.com>
2019-09-18 10:42:22 +0200

    Use a mocked name so we can see a failing test if the route didn't match

--- cypress/integration/cy-server.spec.js
+++ cypress/integration/cy-server.spec.js
@@ -8,6 +8,6 @@ context('Cy Server Randomness', () => {
     cy.route("get", "https://hub.docker.com/api/content/v1/products/images/php", "fixture:php.json")
       .as('dockerPhp');
 
-    cy.get('[data-testid=productHeader_productName]').should('contain', "php");
+    cy.get('[data-testid=productHeader_productName]').should('contain', "php-mocked");
   });
 });
